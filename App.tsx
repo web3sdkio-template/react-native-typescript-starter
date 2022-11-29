73a1db9669d9af57ed02224ce632a82f1f253f23
zendqgoo <zendqgoo@outlook.com>
2022-11-29 11:23:01 +0800

    First commit

---
+++ App.tsx
@@ -0,0 +1,97 @@
+// Step 1: Import the crypto getRandomValues shim (**BEFORE** the ethers shims)
+import "react-native-get-random-values";
+
+// Step 2: Import the ethers shims (**BEFORE** the web3sdkio SDK)
+import "@ethersproject/shims";
+
+// Step 3: Import the web3sdkio SDK
+
+import { NFT, Web3sdkioSDK } from "@web3sdkio/sdk";
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import {
+  Image,
+  Linking,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+export default function App() {
+  const [nfts, setNFTS] = useState<NFT[]>([]);
+  useEffect(() => {
+    const sdk = new Web3sdkioSDK("goerli");
+    const loadNFTS = async () => {
+      const contract = await sdk.getContract(
+        "0x9E7945873C945fB0cdd438c3a9cC55a825C5677D"
+      );
+      return await contract.erc721.getAll();
+    };
+
+    loadNFTS().then((_nfts) => {
+      setNFTS(_nfts);
+    });
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <TouchableOpacity
+          onPress={() => Linking.openURL("https://web3sdk.io")}
+        >
+          <Text style={styles.title}>Welcome to web3sdkio</Text>
+        </TouchableOpacity>
+
+        <View style={styles.grid}>
+          {nfts.map((nft, i) => (
+            <TouchableOpacity
+              onPress={() =>
+                // @ts-ignore
+                Linking.openURL(nft?.metadata?.attributes[i].value)
+              }
+              key={nft.metadata.id}
+            >
+              <Image
+                style={styles.image}
+                source={{
+                  uri: nft.metadata?.image as string,
+                }}
+              />
+            </TouchableOpacity>
+          ))}
+        </View>
+      </View>
+      <StatusBar style="light" />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#0f1318",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#f213a4",
+  },
+  grid: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    maxWidth: "100%",
+    marginTop: 16,
+  },
+  image: {
+    borderRadius: 10,
+    margin: 8,
+    width: 400,
+    height: 75,
+  },
+});
---
+++ README.md
@@ -0,0 +1,31 @@
+# React Native + web3sdkio + TypeScript Starter Template
+
+## 👷🚧 Alpha 🚧👷
+
+This template is in alpha right now. We're working on improving it and adding more features. If you have any feedback, please let us know!
+
+Got questions? Jump into our [Discord](https://discord.com/invite/web3sdkio) to speak with us directly.
+
+## Getting Started
+
+Create a project using this example:
+
+```bash
+npx web3sdkio create --template react-native-typescript-starter
+```
+
+You can start editing the page by modifying `App.jsx`. The page auto-updates as you edit the file.
+
+## Learn More
+
+To learn more about web3sdkio and Next.js, take a look at the following resources:
+
+- [web3sdkio Portal](https://portal.web3sdk.io) - check our guides and development resources.
+- [web3sdkio SDK](https://portal.web3sdk.io/sdk) - check our guides and development resources.
+- [React Native Documentation](https://reactnative.dev/) - learn about Next.js features and API.
+
+You can check out [the web3sdkio GitHub organization](https://github.com/web3sdkio) - your feedback and contributions are welcome!
+
+## Join our Discord!
+
+For any questions, suggestions, join our discord at [https://discord.gg/web3sdkio](https://discord.gg/web3sdkio).
